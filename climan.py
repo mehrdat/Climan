f31d54e837a45ea5942c6d08767a121b0150e4dd
Mehrdad <mehr.lashgari@gmail.com>
2024-05-01 01:24:44 +0100

    added earth

--- climan.py
+++ climan.py
@@ -1,2 +1,14 @@
 from manim import *
 import matplotlib
+from manim import *
+
+class EarthScene(ThreeDScene):
+    def construct(self):
+        earth = Sphere(radius=2, resolution=(50, 50))
+        earth.set_fill(color=BLUE, opacity=1)
+        self.set_camera_orientation(phi=75 * DEGREES, theta=-45 * DEGREES)
+        self.play(ShowCreation(earth))
+        self.wait()
+
+earth_scene = EarthScene()
+earth_scene.render()
